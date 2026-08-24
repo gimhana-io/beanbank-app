c1f7c0095c41346791ff36ac5e27e5aab700dec4
gimhana-io <gimhanad@gmail.com>
2026-08-24 16:33:40 +0530

    project setup

---
+++ src/main/java/com/beanbank/beanbank_app/BeanbankAppApplication.java
@@ -0,0 +1,13 @@
+package com.beanbank.beanbank_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanbankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanbankAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beanbank/beanbank_app/BeanbankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beanbank.beanbank_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanbankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
