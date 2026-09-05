4ec5ee7cb1b9b98e2984e4ba22f9cfa30688dc26
gimhana-io <gimhanad.io@gmail.com>
2026-09-05 06:06:11 +0530

    configure users using InMemoryUserDetailsManager

--- src/main/java/com/beanbank/beanbank_app/config/ProjectSecurityConfig.java
+++ src/main/java/com/beanbank/beanbank_app/config/ProjectSecurityConfig.java
@@ -5,6 +5,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,6 +22,13 @@ public class ProjectSecurityConfig{
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user").password("{noop}123").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}123").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 };
